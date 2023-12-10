dfbc518ee341d4da26404cad8da2f7fabd86e820
AndjelijaMladenovic00 <andjelija.mladenovic@elfak.rs>
2023-12-10 22:16:08 +0100

    Zoomed onto Nis

--- JamScout/app/src/main/java/com/example/jamscout/MapsActivity.java
+++ JamScout/app/src/main/java/com/example/jamscout/MapsActivity.java
@@ -39,13 +39,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        LatLng nis = new LatLng(43.3139854,21.8900026);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(nis, 14));
     }
 }
