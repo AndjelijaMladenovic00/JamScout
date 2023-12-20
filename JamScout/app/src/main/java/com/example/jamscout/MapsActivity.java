61cead5f0e8cae37b465b80c3d8bc4535008239c
AndjelijaMladenovic00 <andjelija.mladenovic@elfak.rs>
2023-12-20 22:33:37 +0100

    Searchbar added.

--- JamScout/app/src/main/java/com/example/jamscout/MapsActivity.java
+++ JamScout/app/src/main/java/com/example/jamscout/MapsActivity.java
@@ -5,9 +5,12 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -22,27 +25,74 @@ import com.example.jamscout.databinding.ActivityMapsBinding;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private final int FINE_PERMISSION_CODE = 1;
+    final LatLng nis = new LatLng(43.3139854, 21.8900026);
     private GoogleMap map;
+    private boolean has_current_location = false;
     private ActivityMapsBinding binding;
     private Location currentLocation = new Location(LocationManager.GPS_PROVIDER);
-    final LatLng nis = new LatLng(43.3139854,21.8900026);
+    private SearchView mapSearchView;
+    private LatLng gotoLocation;
     private FusedLocationProviderClient flpc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        mapSearchView = findViewById(R.id.mapSearch);
 
         currentLocation.setLatitude(43.3139854);
         currentLocation.setLongitude(21.8900026);
 
-        binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        //binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        //setContentView(binding.getRoot());
 
         flpc = LocationServices.getFusedLocationProviderClient(this);
         getLastLocation();
+
+        mapSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String searched_location = mapSearchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if (searched_location != null) {
+                    Geocoder geocoder = new Geocoder(MapsActivity.this);
+
+                    try {
+                        addressList = geocoder.getFromLocationName(searched_location, 1);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    Address address = addressList.get(0);
+                    gotoLocation = new LatLng(address.getLatitude(), address.getLongitude());
+
+                    map.addMarker(new MarkerOptions().position(gotoLocation).title("Vase odrediste!"));
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(gotoLocation, 15));
+
+                    if (!has_current_location) {
+                        return false;
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MapsActivity.this);
     }
 
     private void getLastLocation() {
@@ -56,7 +106,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onSuccess(Location location) {
                 if (location != null) {
                     currentLocation = location;
-
+                    has_current_location = true;
                     SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                             .findFragmentById(R.id.map);
                     mapFragment.getMapAsync(MapsActivity.this);
@@ -85,9 +135,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if(!location.equals(nis)){
             map.addMarker(new MarkerOptions().position(location).title("Vi ste ovde!"));
         }
-
-
-
     }
 
     @Override
